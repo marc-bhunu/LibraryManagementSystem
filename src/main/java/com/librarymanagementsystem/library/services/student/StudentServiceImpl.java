f4b0c0725f294d07c921c0b36d7677d4b251eb9b
marc-bhunu <nomubhunu@gmail.com>
2021-11-06 11:37:51 +0200

    Created Student Service

--- src/main/java/com/librarymanagementsystem/library/services/student/StudentServiceImpl.java
+++ src/main/java/com/librarymanagementsystem/library/services/student/StudentServiceImpl.java
@@ -1,6 +1,7 @@
 package com.librarymanagementsystem.library.services.student;
 
 import com.librarymanagementsystem.library.models.Student;
+import com.librarymanagementsystem.library.repositories.StudentRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -8,28 +9,35 @@ import java.util.List;
 
 @Service
 public class StudentServiceImpl  implements IStudentsService {
+
+    StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     @Override
     public List<Student> list() {
-        return null;
+       return studentRepository.findAll();
     }
 
     @Override
     public Student get(Long id) {
-        return null;
+        return  studentRepository.getById(id);
     }
 
     @Override
     public Student create(Student student) {
-        return null;
+        return  studentRepository.save(student);
     }
 
     @Override
     public void delete(Long id) {
-
+        studentRepository.deleteById(id);
     }
 
     @Override
     public Student update(Student student) {
-        return null;
+        return studentRepository.save(student);
     }
 }
